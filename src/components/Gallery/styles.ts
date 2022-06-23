cb68a662596b306096e838f06087e86e07674042
GL-19 <loiolagl19@gmail.com>
2022-06-23 12:58:32 -0300

    Gallery finished

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -8,19 +8,22 @@ interface GalleryImageProps {
 export const GalleryImage = styled.div<GalleryImageProps>`
 	width: 100%;
 	height: 100%;
-	min-height: 30rem;
-	padding: 5rem;
+	max-width: 25vw;
+	min-height: 25vw;
 
 	background: no-repeat url(${({ desktopBackground }) => desktopBackground});
 	background-size: 100% 100%;
 
 	@media screen and (max-width: 450px) {
-		min-height: 20rem;
+		max-width: 50vw;
+		min-height: 50vw;
 		background-image: url(${({ mobileBackground }) => mobileBackground});
 	}
 `;
 
 export const GalleryContainer = styled.div`
+	max-width: 100%;
+
 	display: grid;
 	grid-template-columns: repeat(4, 1fr);
 
